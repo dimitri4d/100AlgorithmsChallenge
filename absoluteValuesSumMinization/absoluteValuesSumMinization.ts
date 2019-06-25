39426528f95a78870d13321591400467b17bd4c8
Dimitri Frederick <dimitri.frederick@cooperators.ca>
2019-06-24 23:14:15 -0400

    1 - absoluteValuesSumMinimization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 function absoluteValuesSumMinimization(a: number[]): number {
 
+    const length = a.length
+    const center = Math.ceil((length/2)-1)
+
+    return  a[center]
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
